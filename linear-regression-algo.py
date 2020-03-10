5feccb791e1b0a543daf0333cbeec1ecaba79a39
Subramanian <subru1997@gmail.com>
2020-03-10 20:33:46 +0100

    change data type of a input parameter in split_train_test function

--- linear-regression-algo.py
+++ linear-regression-algo.py
@@ -2,13 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def split_train_test(dataset, test_size=20):
+def split_train_test(dataset, test_size=0.25):
     dataset = dataset.sample(frac=1 ,random_state=200)
     X = dataset.iloc[:, :-1].values
     y = dataset.iloc[:, 1].values
     dataset_size = dataset.shape[0]
-    split_index = int(round((dataset_size / 10)*(test_size/10))) 
-    
+    split_index = int(round((dataset_size / 10)*(test_size*10))) 
     return X[split_index:], y[split_index:], X[:split_index], y[:split_index]  
 
     
@@ -17,15 +16,7 @@ def mean_sqaure_error(m, c, X_train, y_train):
     n = len(X_train)
     for i in range(n):    
         error += (y_train[i] - ((m*X_train[i]) + c))**2
-    return (1/float(n))*error
-
-def compute_error_for_line_given_points(b, m, points):
-    totalError = 0
-    for i in range(0, len(points)):
-        x = points[i, 0]
-        y = points[i, 1]
-        totalError += (y - (m * x + b)) ** 2
-    return totalError / float(len(points))
+    return (1/n)*error
 
     
 def gradient_descent(m, c, learning_rate, X, y):
@@ -37,7 +28,6 @@ def gradient_descent(m, c, learning_rate, X, y):
         c_gradient += - (2/n) * (y[i] - ((m * X[i]) + c))
     new_m = m - (m_gradient * learning_rate)
     new_c = c - (c_gradient * learning_rate)
-    
     return new_m, new_c
 
 
@@ -58,7 +48,7 @@ if __name__ == "__main__":
     file_loc = "Salary_Data.csv"
     dataset = pd.read_csv(file_loc)     
     
-    X_train, y_train, X_test, y_test = split_train_test(dataset, 20)
+    X_train, y_train, X_test, y_test = split_train_test(dataset, 0.2)
     
     learning_rate = 0.0001
     initial_m = 0
