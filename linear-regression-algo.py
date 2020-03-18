cde36d34278d0b61c11918251d2808c38955396e
Subramanian <subru1997@gmail.com>
2020-03-18 20:45:28 +0100

    optimize the model in order to converge

--- linear-regression-algo.py
+++ linear-regression-algo.py
@@ -10,6 +10,21 @@ def split_train_test(dataset, test_size=0.25):
     split_index = int(round(dataset_size * test_size)) 
     return X[split_index:], y[split_index:], X[:split_index], y[:split_index]  
 
+
+def matrix_regressor(x, y):
+    X_t = []
+    for i in x:
+        X_t.extend(i)
+    X = np.vstack((X_t, np.ones(len(X_t))))
+    # print(x, X)
+    Y = []
+    for i in y:
+        Y.append([i])
+    # print(y, Y)
+#    print(X, Y)
+    A = np.linalg.inv(X @ X.T) @ X @ Y
+    return A
+
     
 def mean_sqaure_error(m, c, X_train, y_train):
     error = 0
@@ -22,20 +37,31 @@ def mean_sqaure_error(m, c, X_train, y_train):
 def gradient_descent(m, c, learning_rate, X, y):
     m_gradient = 0
     c_gradient = 0
+    convergance = False
     n = len(X)
     for i in range(n):
         m_gradient += - (2/n) * X[i] * (y[i] - ((m * X[i]) + c))
         c_gradient += - (2/n) * (y[i] - ((m * X[i]) + c))
+    
+    if (-500 < m_gradient < 500) and ( -500< c_gradient < 500):
+        print("Grad", m_gradient, c_gradient)
+        convergance = True
+    else:
+        learning_rate = 0.001
+        
     new_m = m - (m_gradient * learning_rate)
     new_c = c - (c_gradient * learning_rate)
-    return new_m, new_c
+    return new_m, new_c, convergance
 
 
 def build_model(initial_m, initial_c, learning_rate, X, y, num_iterations):
     m = initial_m
     c = initial_c
-    for _ in range(num_iterations):
-        m,c = gradient_descent(m, c, learning_rate, X, y)
+    convergance = False
+#    for _ in range(num_iterations):
+#        m,c = gradient_descent(m, c, learning_rate, X, y)
+    while not convergance:
+        m,c, convergance = gradient_descent(m, c, learning_rate, X, y)
     return [m,c]
 
 
@@ -59,5 +85,22 @@ if __name__ == "__main__":
     print(mean_sqaure_error(initial_m, initial_c, X_train, y_train))
     print(mean_sqaure_error(model[0], model[1], X_train, y_train))
     
-    for i in range(len(X_test)):
+    for i in range(len(X_test)):        
         print(f"Predicted values is {predict(model, X_test[i])}, actual value is {y_test[i]}")
+        
+    mat_reg = matrix_regressor(X_train, y_train)
+    
+    y_pred = []
+    for i in range(len(X_train)):        
+        y_pred.append(predict(model, X_train[i]))
+
+    print(model)
+    print(mat_reg)
+        
+    # Visualising the Training set results
+    plt.scatter(X_train, y_train, color = 'red')
+    plt.plot(X_train, y_pred, color = 'blue')
+    plt.title('Salary vs Experience (Training set)')
+    plt.xlabel('Years of experience')
+    plt.ylabel('Salary')
+    plt.show()
